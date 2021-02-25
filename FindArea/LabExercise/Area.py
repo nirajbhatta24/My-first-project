05c4e15b8e73401222558757893836b052b423ac
Hp <nirajbhatta108@gmail.com>
2021-02-25 10:14:45 +0545

    commitment

---
+++ FindArea/LabExercise/Area.py
@@ -0,0 +1,9 @@
+# Write a program that reads the length of the base and the height of the right-angled triangle and print the area.
+# Every number is given on a saparate line
+
+
+
+length = int(input("enter a value of length:"))
+height = int(input("enter a value of breadth:"))
+area = (length + height)//2
+print(f"The area of right angled triangle is {area}")
